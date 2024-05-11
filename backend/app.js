e23122ad7a2187db9a559a89fe4c9f484148e5e3
Danny Virasawmi <danv100@hotmail.com>
2024-05-11 17:34:30 -0400

    38 complete

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fadf12421l',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: 'asdf52sd23',
+      title: 'Second server-side post',
+      content: 'This is coming from the server also!'
+    },
+    {
+      id: 'yrt56fgh34',
+      title: 'Third server-side post',
+      content: 'This is coming also from the server'
+    },
+    {
+      id: 'frth43rf23',
+      title: 'Fourth server-side post',
+      content: 'This too is coming from the server'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
