b00e9666c029e221402d5cf31dc59ca63a89340c
Danny Virasawmi <danv100@hotmail.com>
2024-05-11 11:06:59 -0400

    24 complete

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrl: './post-list.component.scss'
+})
+export class PostListComponent {
+  // posts = [
+  //   {title: 'First Post', content: "This is the first post's content"},
+  //   {title: 'Second Post', content: "This is the second post's content"},
+  //   {title: 'Third Post', content: "This is the third post's content"},
+  // ];
+  @Input() posts = [];
+}
