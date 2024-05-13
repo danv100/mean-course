dec37d4a07fd0a9ff0b7ea6536ee55276b2ff252
Danny Virasawmi <danv100@hotmail.com>
2024-05-12 20:54:49 -0400

    55 complete

--- backend/app.js
+++ backend/app.js
@@ -36,32 +36,13 @@ app.post("/api/posts", (req, res, next) => {
 })
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fadf12421l',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: 'asdf52sd23',
-      title: 'Second server-side post',
-      content: 'This is coming from the server also!'
-    },
-    {
-      id: 'yrt56fgh34',
-      title: 'Third server-side post',
-      content: 'This is coming also from the server'
-    },
-    {
-      id: 'frth43rf23',
-      title: 'Fourth server-side post',
-      content: 'This too is coming from the server'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
