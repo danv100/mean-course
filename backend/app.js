d002214b774989c960d9ff835edbfb4e9fc24363
Danny Virasawmi <danv100@hotmail.com>
2024-05-12 21:57:30 -0400

    57 corrected and completed

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,7 @@ app.get('/api/posts', (req, res, next) => {
     });
 });
 
-app.delete("/app/posts/:id", (req, res, next) => {
+app.delete("/api/posts/:id", (req, res, next) => {
   Post.deleteOne({_id: req.params.id})
     .then(result => {
       console.log(result);
