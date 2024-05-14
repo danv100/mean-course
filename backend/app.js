e38118368d55f37012b14467ae97b9b328c98f95
Danny Virasawmi <danv100@hotmail.com>
2024-05-13 21:49:17 -0400

    83 completed

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
@@ -16,6 +17,7 @@ mongoose.connect("mongodb+srv://danv100:1goodDatabase!@cluster0.tcong8h.mongodb.
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
