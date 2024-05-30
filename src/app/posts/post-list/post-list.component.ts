8857a2ae7153b8ee972d1f13ffc7be48e04b1a65
Danny Virasawmi <danv100@hotmail.com>
2024-05-30 13:08:05 -0400

    119 complete

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -23,6 +23,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   currentPage=1;
   pageSizeOptions = [1, 2, 5, 10];
   userIsAuthenticated: boolean = false;
+  userId: string;
   private postsSub: Subscription;
   private authStatusSub: Subscription;
 
@@ -31,6 +32,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.isLoading = true;
     this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    this.userId = this.authService.getUserId();
     this.postsSub = this.postsService
       .getPostUpdateListener()
       .subscribe((postData: {posts: Post[], postCount: number}) => {
@@ -41,6 +43,7 @@ export class PostListComponent implements OnInit, OnDestroy {
       this.userIsAuthenticated = this.authService.getIsAuth();
       this.authStatusSub = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
         this.userIsAuthenticated = isAuthenticated;
+        this.userId = this.authService.getUserId();
       });
   }
 
