bf9cb8e26128f17f2a5208434c46e6462b87561c
Danny Virasawmi <danv100@hotmail.com>
2024-06-02 14:09:42 -0400

    148 complete

--- backend/app.js
+++ backend/app.js
@@ -18,16 +18,20 @@ mongoose.connect("mongodb+srv://danv100:" + process.env.MONGO_ATLAS_PW + "@clust
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use("/images", express.static(path.join("images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/", express.static(path.join(__dirname, 'angular//browser')));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+//   next();
+// });
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
+app.use((res, req, next) => {
+  req.sendFile(path.join(__dirname, 'angular\\browser', "index.html"));
+});
 
 module.exports = app;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000/api"
+  apiUrl: "http://danny-board-02-env.eba-fuwavbdq.us-east-2.elasticbeanstalk.com/api"
 };
